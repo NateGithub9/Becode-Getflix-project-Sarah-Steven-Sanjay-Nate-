a89c13034aac29f0cef0ecb62abfd7aaa18c88b5
sabbels <belhadj.sarah@outlook.com>
2024-07-31 12:02:16 +0200

    add break

--- profil.php
+++ profil.php
@@ -50,10 +50,10 @@
         <h2>Nouveau compte</h2>
         <form action="/login" method="post">
 
-            <label for="email">Adresse e-mail:</label>
+            <label for="email">Adresse e-mail:</label><br>
             <input type="text" id="new-mail" name="email" required>
 
-            <label for="password">Mot de passe:</label>
+            <label for="password">Mot de passe:</label><br>
             <input type="text" id="password" name="password" required>
 
             <input type="submit" value="Créer">
