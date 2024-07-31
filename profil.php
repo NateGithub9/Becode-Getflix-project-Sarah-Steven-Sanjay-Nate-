caa090ad5b78c3547d12bf6b6c14d35f54544b83
sabbels <belhadj.sarah@outlook.com>
2024-07-31 11:32:32 +0200

    form new compte

--- profil.php
+++ profil.php
@@ -37,8 +37,8 @@
     <div class="login-container">
         <h2>Login</h2>
         <form action="/login" method="post">
-            <label for="username">Nom d'utilisateur:</label>
-            <input type="text" id="username" name="username" required>
+            <label for="email">Adresse e-mail:</label>
+            <input type="text" id="email" name="email" required>
 
             <label for="password">Mot de passe:</label>
             <input type="password" id="password" name="password" required>
@@ -49,14 +49,14 @@
     <div class="new-account-container">
         <h2>Nouveau compte</h2>
         <form action="/login" method="post">
-            <label for="username">Nom d'utilisateur:</label>
-            <input type="text" id="new-username" name="username" required>
 
             <label for="email">Adresse e-mail:</label>
-            <input type="text" id="email" name="email" required>
+            <input type="text" id="new-mail" name="email" required>
 
             <label for="password">Mot de passe:</label>
-            <input type="text">
+            <input type="text" id="password" name="password" required>
+
+
         </form>
 
 
