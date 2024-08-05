26de8e928f060811ffe76611a34afb418f010781
Sanjay L <160046631+Kendrak90@users.noreply.github.com>
2024-08-05 12:18:36 +0200

    Update bouton creation compte

--- profil.php
+++ profil.php
@@ -5,12 +5,12 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Streaming Website</title>
+    <title>Streaming Website - Profil</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 </head>
@@ -19,7 +19,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="#"></a>
-        <img src="images/logo.png" alt="logo" title="logo" width="180" height="39">
+        <a href="index.php"><img src="images/logo.png" alt="logo" title="logo" width="180" height="39"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -43,33 +43,7 @@
             </ul>
         </div>
     </nav>
-    <!--<div class="login-container">
-        <h2>Login</h2>
-        <form action="/login" method="post">
-            <label for="email">Adresse e-mail:</label>
-            <input type="text" id="email" name="email" required>
 
-            <label for="password">Mot de passe:</label>
-            <input type="password" id="password" name="password" required>
-
-            <input type="submit" value="Login">
-        </form>
-    </div>
-    <div class="new-account-container">
-        <h2>Nouveau compte</h2>
-        <form action="/login" method="post">
-
-            <label for="email">Adresse e-mail:</label>
-            <input type="text" id="new-mail" name="email" required>
-
-            <label for="password">Mot de passe:</label>
-            <input type="text" id="password" name="password" required>
-
-            <input type="submit" value="Créer">
-
-        </form>
-
-    </div>-->
     <section class="vh-100" style="background-color: #111;">
         <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
@@ -79,17 +53,16 @@
 
                             <h3 class="mb-5">Connecte-toi</h3>
 
-                            <div data-mdb-input-init class="form-outline mb-4">
+                            <div class="form-outline mb-4">
                                 <input type="email" id="typeEmailX-2" class="form-control form-control-lg" />
                                 <label class="form-label" for="typeEmailX-2">E-mail</label>
                             </div>
 
-                            <div data-mdb-input-init class="form-outline mb-4">
+                            <div class="form-outline mb-4">
                                 <input type="password" id="typePasswordX-2" class="form-control form-control-lg" />
                                 <label class="form-label" for="typePasswordX-2">Mot de passe</label>
                             </div>
 
-                            <!-- Checkbox -->
                             <div class="form-check d-flex justify-content-start mb-4">
                                 <input class="form-check-input" type="checkbox" value="" id="form1Example3" />
                                 <label class="form-check-label" for="form1Example3"> Se rappeler du mot de passe </label>
@@ -101,18 +74,25 @@
 
                             <button data-mdb-button-init data-mdb-ripple-init class="btn btn-lg btn-block btn-primary" style="background-color: #dd4b39;" type="submit"><i class="fab fa-google me-2"></i> Se connecter avec Google</button>
                             <button data-mdb-button-init data-mdb-ripple-init class="btn btn-lg btn-block btn-primary mb-2" style="background-color: #3b5998;" type="submit"><i class="fab fa-facebook-f me-2"></i>Se connecter avec Facebook</button>
+                            
+                            <!-- Bouton Inscription -->
+                            <button data-mdb-button-init data-mdb-ripple-init class="btn btn-lg btn-block btn-secondary mt-2" style="background-color: #6c757d;" onclick="window.location.href='formulaireinscription.php'">Créer un compte</button>
 
                         </div>
                     </div>
                 </div>
             </div>
-            <footer class="footer">
-                Website created by Sarah, Steven, Sanjay & Nate. Check out our source code!
-                <a href="https://github.com/NateGithub9/Becode-Getflix-project-Sarah-Steven-Sanjay-Nate-" target="_blank"><img src="images/git.webp" width="50" height="50" alt="github icon"></a>
-            </footer>
-            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        </div>
+    </section>
+
+    <footer class="footer text-center">
+        Website created by Sarah, Steven, Sanjay & Nate. Check out our source code!
+        <a href="https://github.com/NateGithub9/Becode-Getflix-project-Sarah-Steven-Sanjay-Nate-" target="_blank"><img src="images/git.webp" width="50" height="50" alt="github icon"></a>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
