5a520c56c660d08e54178825ed27d2e420eb5859
NateGithub9 <nathaniel.akingbade@gmail.com>
2024-08-07 15:11:25 +0200

    test3

--- configdb.php
+++ configdb.php
@@ -1,10 +1,10 @@
 <?php
    // Récupérer l'URL de la base de données depuis les variables d'environnement
    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-   $cleardb_server = $cleardb_url["DATABASE_HOST"];
-   $cleardb_username = $cleardb_url["DATABASE_USER"];
-   $cleardb_password = $cleardb_url["DATABASE_PASSWORD"];
-   $cleardb_db = substr($cleardb_url["DATABASE_NAME"],1);
+   $cleardb_server = $cleardb_url["host"];
+   $cleardb_username = $cleardb_url["user"];
+   $cleardb_password = $cleardb_url["pass"];
+   $cleardb_db = substr($cleardb_url["path"],1);
 
    // Connexion à la base de données
    try {
